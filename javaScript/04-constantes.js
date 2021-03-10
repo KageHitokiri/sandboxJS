158c68d53dbaee3aecb72955e942338770124d98
Carlos <skhcri@gmail.com>
2021-03-10 22:33:00 +0100

    Script 4: Constantes

---
+++ javaScript/04-constantes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var web = "https://github.com/KageHitokiri";
+const ip = "192.168.1.1";
+console.log(web);
+console.log(ip);
+ip = "ASD";
+web = "https://www.templatemonster.com/help/es/developer-tools.html";
+console.log(web);
+/* Error de código
+console.log(ip);
+*/

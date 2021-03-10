33abaec68f100d5a9a7ea0f10c84a2fb29bc4b23
Carlos <skhcri@gmail.com>
2021-03-10 23:01:04 +0100

    Script 5, tipos de datos y operaciones básicas

---
+++ javaScript/05-tiposdeDatos.js
@@ -0,0 +1,17 @@
+"use strict"
+
+var num1 = 5;
+var num2 = 8;
+var operacion = num1-num2;
+
+console.log(operacion);
+
+var numeroEntero = 50;
+var cadenaDeTexto = "hola mundo!"
+var verdaderoOFalso = true;
+
+var numeroFalso ="33";
+
+console.log(numeroFalso+7+" sin parseInt");
+console.log(parseInt(numeroFalso)+7+" con parseInt");
+console.log(String(numeroEntero)+" Texto concatenado");

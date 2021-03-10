d9c6cbb8cf6530437c8fe410f644464ab8590a40
Carlos <skhcri@gmail.com>
2021-03-10 22:24:36 +0100

    Script let y var

---
+++ javaScript/03-letYVar.js
@@ -0,0 +1,22 @@
+'use strict'
+
+//Test var
+var numero = 5;
+console.log(numero);
+
+if (true) {
+    numero = 7;
+    console.log(numero);
+}
+
+console.log(numero);
+//Test Let
+let texto = "Test";
+console.log(texto);
+
+if (true) {
+    let texto ="Prueba";
+    console.log(texto);
+}
+
+console.log(texto);

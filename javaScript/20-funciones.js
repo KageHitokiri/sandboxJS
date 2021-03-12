399068f404480ba9230d68ef1a08732fd98614f6
Carlos <skhcri@gmail.com>
2021-03-12 14:13:07 +0100

    Funciones

---
+++ javaScript/20-funciones.js
@@ -0,0 +1,15 @@
+"use strict"
+
+/**
+ * Funciones
+ */
+
+function calculadora(){
+    console.log("Soy la calculadora");
+    return "Esto es una prueba";
+}
+
+calculadora();
+
+let test = calculadora();
+console.log(test);

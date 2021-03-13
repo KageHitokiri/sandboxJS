bee4df7cfb318a2849dbe74b245ba0091c0abb64
Carlos <skhcri@gmail.com>
2021-03-13 02:28:39 +0100

    Script de Arrays

---
+++ javaScript/25-arrays.js
@@ -0,0 +1,13 @@
+"use strict"
+
+/**
+ * Arrays
+ */
+
+var nombre = "Carlos";
+var nombres = ["Carlos", "Saúl", "Andrea", "Aarón"];
+var lenguajes = new Array("Java","JavaScript","TypeScript");
+
+console.log(nombre);
+console.log(nombres[nombres.length-1]);
+console.log(lenguajes[lenguajes.length-1]);

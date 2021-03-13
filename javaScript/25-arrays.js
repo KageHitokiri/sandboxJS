2590828657c55721fce06237c8f403094f7eca4b
Carlos <skhcri@gmail.com>
2021-03-13 02:44:34 +0100

    Arrays avanzados

--- javaScript/25-arrays.js
+++ javaScript/25-arrays.js
@@ -5,9 +5,22 @@
  */
 
 var nombre = "Carlos";
-var nombres = ["Carlos", "Saúl", "Andrea", "Aarón"];
+var nombres = ["Carlos", "Saúl", "Andrea", "Aarón","Antonio"];
 var lenguajes = new Array("Java","JavaScript","TypeScript");
 
 console.log(nombre);
 console.log(nombres[nombres.length-1]);
 console.log(lenguajes[lenguajes.length-1]);
+/*
+let elemento;
+
+do {
+    elemento = parseInt(prompt("Introduce un número entre 0 y 3",0));
+} while (elemento<=0 && elemento>=3);
+
+alert(nombres[elemento]);
+*/
+document.write("Los alumnos son<br/>");
+for (let i = 0; i<nombres.length;i++) {
+    document.write("<li>"+nombres[i]+"</li>");
+}

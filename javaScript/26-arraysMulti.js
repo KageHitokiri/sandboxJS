f9f9ed5904c00ff238cdd78443f0cd18a3fffa00
Carlos <skhcri@gmail.com>
2021-03-13 03:13:29 +0100

    Operaciones básicas con Arrays

--- javaScript/26-arraysMulti.js
+++ javaScript/26-arraysMulti.js
@@ -2,7 +2,35 @@
 
 let categorias = ["Acción", "Terror", "RPG"];
 let titulos = ["Final Fantasy 14", "Golden Sun", "Silent Hill"];
-
 let juegos = [categorias, titulos];
-
+let titulo;
+let juegosString;
+/*
 console.log(juegos[1][2]);
+*/
+juegos.push("GoldenEye");
+console.log(juegos);
+
+/*
+do {
+    titulo = prompt(`
+    Introduce un juego en el array<br>
+    Introduce un número para terminar
+    `);
+    if(isNaN(titulo)) {
+        juegos.push(titulo);
+    }
+}while (isNaN(titulo));
+console.log(juegos);
+juegos.pop();
+console.log(juegos);
+*/
+
+var index = titulos.indexOf("Final Fantasy 14");
+console.log(index);
+console.log(titulos);
+if (index > -1) {
+    titulos.splice(index,1);
+}
+juegosString = juegos.join();
+console.log(juegosString);

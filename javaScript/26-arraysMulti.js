f8d7ae6cfd520f175b5db4b0d9126f9a8db6094a
Carlos <skhcri@gmail.com>
2021-03-13 02:56:22 +0100

    ArraysMultiples script

---
+++ javaScript/26-arraysMulti.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let categorias = ["Acción", "Terror", "RPG"];
+let titulos = ["Final Fantasy 14", "Golden Sun", "Silent Hill"];
+
+let juegos = [categorias, titulos];
+
+console.log(juegos[1][2]);

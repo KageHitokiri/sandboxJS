a646d18db46692e15f22217b302b027aa2cf03f7
Carlos <skhcri@gmail.com>
2021-03-13 22:13:14 +0100

    Crear promesas

--- javaScript/Peticiones Asincronas/fetch.js
+++ javaScript/Peticiones Asincronas/fetch.js
@@ -5,16 +5,42 @@ window.addEventListener("load",()=>{
      * Fetch y peticiones a servicios / apis REST
      */
     var usersDiv;
+    var studentDiv;
     usersDiv= document.querySelector("#usersContainer");
-        
+    studentDiv = document.querySelector("#studentContainer");
+
     getUsers() 
         .then(data => data.json())
         .then(data => {
-            listedUsers(data)            
-    }) 
+            listedUsers(data);
+            return getInfo();            
+    })
+    .then(data => {
+        console.log(data);
+        studentDiv.innerHTML = data;
+    })
 
     function getUsers(url ="https://jsonplaceholder.typicode.com/users"){
         return fetch(url);
+    }
+
+    function getInfo(){
+        let alumno = {
+            nombre: "Carlos",
+            apellidos: "Ramos",
+            url: null}
+
+            return new Promise((resolve, reject)=>{
+                let alumnoString;
+                
+                setTimeout(()=>{
+                    alumnoString = JSON.stringify(alumno);
+                if (typeof alumnoString!= "string") {
+                    return reject("Error!");
+                } 
+                return resolve(alumnoString);
+                }, 1000);                
+        });
     }
 
     function listedUsers(users) {

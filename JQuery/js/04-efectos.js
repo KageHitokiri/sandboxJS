e2168f6eb14340714de7e853afe8e9f4a6ddc0f0
Carlos <skhcri@gmail.com>
2021-03-15 06:24:53 +0100

    Animaciones básicas de jQuery

---
+++ JQuery/js/04-efectos.js
@@ -0,0 +1,51 @@
+'use strict'
+
+window.addEventListener('load',()=>{
+    let vanishContainer;
+    vanishContainer = $('#vanishContainer');
+
+    $('#showButton').hide();
+    /**
+     * .show(tiempo) 
+     *  Muestra en el tiempo dado
+        * tiempo = tiempo en milisegundos o en string
+        * =>slow, fast, normal
+        * =>Por defecto tiempo = 0;
+     * .hide() 
+     *  Oculta
+     * .fadeIn() 
+     *  Muestra mediante desvanecimiento
+     * .fadeOut() 
+     *  Oculta mediante desvanecimiento
+     * .fadeTo(tiempo, opacidad)
+     *  Realiza una transición a la opacidad seleccionada en el tiempo establecido
+        * opacidad = 0min, 1max
+     */
+    $('#showButton').click(function(){
+        //El propio botón $(this) en esta función, se oculta
+        $(this).hide();
+        $('#hideButton').show();
+        //vanishContainer.show("slow");
+        //vanishContainer.fadeIn('slow');
+        vanishContainer.fadeTo(1000, 0.8);
+    });
+
+    $('#hideButton').click(function(){
+        $(this).hide();
+        $('#showButton').show();
+        //vanishContainer.hide("fast");
+        vanishContainer.fadeTo(200, 0.1);
+    })
+    /**
+     * .toggle()
+     * .fadeToggle()
+     * .slideToggle()
+        * slideUp()
+        *   Oculta
+        * SlideDown()
+        *   Muestra
+     */
+    $('#allInOneButton').click(function(){
+        vanishContainer.toggle('fast');
+    });
+})

8358d4fc040fa89634172423684d4ab8c68330c7
Carlos <skhcri@gmail.com>
2021-03-16 22:18:12 +0100

    comentarios

--- Sandbox/js/test.js
+++ Sandbox/js/test.js
@@ -11,18 +11,22 @@ window.addEventListener('load',()=>{
         itFollowsContainer.show();
         followingShadow();
     });
-
+/**
+ * Buscar una forma de reiniciar el estado del cursor.
+ */
     itFollowsHideShadow.click(function(){
         $(this).hide();
         itFollowsShowShadow.show();
         itFollowsContainer.hide();
+        //$('body').css('cursor','pointer');
     })
 
     function followingShadow(){
         $(document).mousemove(function(){
+            //$('body').css('cursor','none');
             itFollowsContainer.css('left',event.clientX)
                             .css('top',event.clientY);
         })
     }
-    
+
 })

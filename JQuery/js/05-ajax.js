c43954a8c6f91d5d8c23bf9fe6eb63ca88f5ff17
Carlos <skhcri@gmail.com>
2021-03-18 15:58:58 +0100

    Peticiones $.ajax (Sale mal)

--- JQuery/js/05-ajax.js
+++ JQuery/js/05-ajax.js
@@ -26,21 +26,62 @@ $(document).ready(()=>{
      * Post
      */
 
-    $('#postForm').submit(()=>{
+    $('#postForm').submit((e)=>{
+        e.preventDefault();
+        
         var user = {
             name    : $('#nameInputText').val(),
             web     : $('#webInputText').val()
         }
         //console.log(user);
-
+        /*
         $.post('https://reqres.in/api/users?page=2', user, function(response){
             console.log(response);
         });
+        */
+    /**
+     * $.ajax
+     * type: 
+     *  tipo de petición
+     * url: 
+     *  Enlace al que se realiza la petición
+     * dataType: 
+     *  tipo de los datos
+     * data: 
+     *  variable de datos
+     * beforeSend:()=> 
+     *  Se realiza antes de realizar la petición
+     * success:()=> 
+     *  Se ejecuta cuando la petición se ha realizado
+     * error:()=> 
+     *  Se ejecuta si hay algun error en la petición
+     * timeout:number
+     *  number=Valor en milisegundos
+     *  Tiempo maximo para que se resuelva la petición
+     *  Si transcurre ese tiempo, lanza error
+     */
+        $.ajax({
+            type        :'POST',
+            url         :$(this).attr('action'),
+            data        :user,
+            beforeSend  :function(){
+                console.log("Enviando usuario...");
+            },
+            success     :function(response){
+                console.log(response);
+            },
+            error       :function(){
+                console.log("Se ha liado, pollito");
+            },
+            timeout     :2000
+        })
         /**
          * Añadimos un return false para evitar que nos redirija
          * De no estar, nos redirigirá a reqres
          */
+
         return false;
     });
+
 
 })

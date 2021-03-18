36af5b4760a5adb7a4846a5786e66ac9766073c2
Carlos <skhcri@gmail.com>
2021-03-18 15:00:19 +0100

    Método get para extraer información

--- JQuery/js/05-ajax.js
+++ JQuery/js/05-ajax.js
@@ -10,6 +10,19 @@ $(document).ready(()=>{
      * divID.load('enlace');
      */
 
-    $('#containerBox').load('https://reqres.in/');
+    //$('#containerBox').load('https://reqres.in/');
+    /**
+     * Get
+     */
 
+    $.get('https://reqres.in/api/users?page=2', {page:3}, function(response){
+        console.log(response);
+        response.data.forEach((element, index)=>{
+            $('#containerBox').append("<p>"+(index+1)+": "+element.first_name+" "+element.last_name+". Email: "+element.email+"</p>");
+        });
+    });
+
+    /**
+     * Post
+     */
 })

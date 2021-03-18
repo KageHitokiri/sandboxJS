40b1d5c943610298ec0afc926d04f3cde10ac945
Carlos <skhcri@gmail.com>
2021-03-18 14:44:27 +0100

    Incrustamos mediante la función load

---
+++ JQuery/js/05-ajax.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/**
+ * https://reqres.in/
+ */
+
+$(document).ready(()=>{
+    /**
+     * Load
+     * divID.load('enlace');
+     */
+
+    $('#containerBox').load('https://reqres.in/');
+
+})
